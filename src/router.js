4051c2ed0c418f6eb1353315e1d4b14f341ca0b3
Pranjal Alone <pranjala@mkcl.org>
2021-09-03 11:09:48 +0530

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+function loadView (view) {
+  return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`)
+}
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      alias: "/",
+      name: 'login',
+      component: loadView('Login'),
+      meta: { title: 'Login', lang: 'mr', icon : 'mdi mdi-account' }
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: loadView('Registration'),
+      meta: { title: 'Registration', lang: 'mr', icon : 'mdi mdi-account-edit' }
+    },
+  ]
+})
